a8486f04ca5c141b546f76694b0be14aec4e4f73
空巷一人 <1530688385@qq.com>
2024-01-09 13:13:32 +0800

    格式化

--- src/logic/base.ts
+++ src/logic/base.ts
@@ -1,5 +1,5 @@
 import validator from 'validator';
-validator.toString =  (input: any) => {
+validator.toString = (input: any) => {
   return String(input);
 };
 export default class extends think.Logic {
