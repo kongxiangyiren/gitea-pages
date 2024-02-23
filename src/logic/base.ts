f95a3ff821c456b371d9145c219d765fee972f6c
空巷一人 <1530688385@qq.com>
2024-02-23 08:47:16 +0800

    更新

--- src/logic/base.ts
+++ src/logic/base.ts
@@ -1,7 +1,3 @@
-import validator from 'validator';
-validator.toString = (input: any) => {
-  return String(input);
-};
 export default class extends think.Logic {
   __before() {
     this.header('X-Powered-By', 'gitea-pages');
